f3801570ffbc763d8ad7ece522721e6a8bd2df29
sitong <1107262334@qq.com>
2026-04-02 14:24:03 +0800

    fix: 深度优化部署策略，对齐仓库基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages
+  base: '/zytx/', // 这里的名称必须和您的 GitHub 仓库名一致
 })
